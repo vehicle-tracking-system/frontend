33299858977a3c74ca6ab88c6b4f74087eb8fce7
Matěj Jehlička <jehlicka@mjehlicka.eu>
2021-03-29 23:01:45 +0200

    View for trackers editing

--- src/views/Trackers.vue
+++ src/views/Trackers.vue
@@ -1,51 +1,98 @@
 <template lang="pug">
   div
-    v-row(no-gutters)
-      v-col(cols="12" md="4" xs="12" sm="12")
-        div(class="text-center")
-          v-progress-circular(
-            v-if="vehiclesLoading"
-            class="text-center"
-            indeterminate
-            :size=50
-            color="primary")
-        VList(
-          v-if="!vehiclesLoading"
-          :items="vehicles"
-        )
-      v-col(cols="12" offset-md="1" md="6" xs="12" sm="12")
-        VList(
-          v-if="!vehiclesLoading"
-          :items="vehicles"
-        )
+    div(class="text-center")
+    TrackerList(
+      v-if="!trackersLoading"
+      :items="trackers"
+      :vehicles="vehicles"
+      @newTracker="persistTracker"
+      @deleteTracker="deleteTracker"
+      @updateTracker="updateTracker"
+      @revokeToken="revokeToken"
+    )
+      v-progress-circular(
+        v-if="trackersLoading"
+        class="text-center"
+        indeterminate
+        :size=50
+        color="primary"
+      )
 </template>
 
 <script>
-import * as api from '@/api'
-import VList from '@/components/VehiclesList'
+import * as api from '@/api/tracker'
+import * as vehicleApi from '@/api/vehicle'
+import TrackerList from '@/components/TrackerList'
 
 export default {
   name: 'Trackers',
   components: {
-    VList
+    TrackerList
   },
   data () {
     return {
+      trackersLoading: true,
       vehiclesLoading: true,
+      trackers: [],
       vehicles: []
     }
   },
   created () {
+    this.loadTrackerList()
     this.loadVehicleList()
   },
   methods: {
+    async loadTrackerList () {
+      api.getAllTrackers(this.$store.getters.StateToken).then(r => {
+        this.trackers = r
+        this.trackersLoading = false
+      }).catch(err => {
+        if (err.status === -1) this.loadingErr = true
+        this.$snotify.error(err.message)
+      })
+    },
     async loadVehicleList () {
-      api.vehicleList(this.$store.getters.StateToken).then(r => {
+      vehicleApi.vehicleList(this.$store.getters.StateToken).then(r => {
         this.vehicles = r
         this.vehiclesLoading = false
       }).catch(err => {
         if (err.status === -1) this.loadingErr = true
         this.$snotify.error(err.message)
+      })
+    },
+    async persistTracker (tracker) {
+      api.newTracker(tracker, this.$store.getters.StateToken).then(r => {
+        this.trackers.push(r)
+      }).catch(e => {
+        this.$snotify.error('Creating tracker error: ' + e.toString())
+        console.error(e)
+      })
+    },
+    async updateTracker (tracker, index) {
+      api.updateTracker(tracker, this.$store.getters.StateToken).then(updated => {
+        Object.assign(this.trackers[index], updated)
+        this.$snotify.success('Tracker ' + tracker.name + ' successfully updated.')
+      }).catch(e => {
+        this.$snotify.error('Updating tracker error: ' + e.toString())
+        console.error(e)
+      })
+    },
+    async deleteTracker (tracker, index) {
+      api.deleteTracker(tracker, this.$store.getters.StateToken).then(_ => {
+        this.$snotify.success('Tracker ' + tracker.name + ' successfully deleted.')
+      }).catch(e => {
+        this.$snotify.error('Updating tracker error: ' + e.toString())
+        console.error(e)
+      })
+    },
+    async revokeToken (tracker, index) {
+      api.revokeToken(tracker, this.$store.getters.StateToken).then(updated => {
+        console.log(index)
+        Object.assign(this.trackers[index], updated)
+        this.$snotify.success(tracker.name + ' token successfully revoked.')
+      }).catch(e => {
+        this.$snotify.error('Updating tracker error: ' + e.toString())
+        console.error(e)
       })
     }
   }
